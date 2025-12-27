c14ce568ddfd9526bc8c53ad24e1e99874dd85bd
Kamil Szerszen <szerszenk1997@gmail.com>
2025-12-27 20:09:00 +0100

    created - Reward entity

--- src/main/java/project/bioinformatics/model/Team.java
+++ src/main/java/project/bioinformatics/model/Team.java
@@ -1,9 +1,22 @@
 package project.bioinformatics.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,6 +25,8 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
+@SQLDelete(sql = "UPDATE exercises SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "teams")
 public class Team {
 
@@ -36,6 +51,9 @@ public class Team {
 
     @Column(nullable = false, updatable = false)
     private Instant createdAt;
+
+    @Column(nullable = false, name = "is_deleted")
+    private boolean isDeleted = false;
 
     @PrePersist
     protected void onCreate() {
